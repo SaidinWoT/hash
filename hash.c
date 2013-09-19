b2f47c8496377f47a18f2d9f961afe8b07e9716b
SaidinWoT <SaidinWoT@gmail.com>
2013-09-19 15:44:44 -0400

    basic hash table

---
+++ hash.c
@@ -0,0 +1,136 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash.h"
+
+#define SIZE 8
+#define LOAD 0.65
+#define COLLIDE(i,s) (((5 * i) + 1) % s)
+
+void hset(Table *t, char *k, char *v);
+
+Table *halloc(unsigned int size) {
+    Table *t = malloc(sizeof(Table));
+    t->size = size;
+    t->load = LOAD;
+    t->entries = 0;
+    t->e = calloc(size, sizeof(Entry *));
+    return t;
+}
+
+void hresize(unsigned int size, Table *t) {
+    Entry **e = t->e;
+    unsigned int s = t->size;
+    t->size = size;
+    t->entries = 0;
+    t->e = calloc(size, sizeof(Entry *));
+    while(--s) {
+        if(e[s] && e[s]->key) {
+            hset(t, e[s]->key, e[s]->val);
+        }
+    }
+}
+
+unsigned int hash(const char *k) {
+    unsigned int t = 0x1523;
+    for(; *k; ++k) {
+        t ^= *k;
+        t *= *k;
+        t ^= *k;
+        t += *k;
+    }
+    return t;
+}
+
+unsigned int _get_index_by_key(Table *t, char *k) {
+    unsigned int i = hash(k) % t->size;
+    mvprintw(0, 40, "%2d", i);
+    mvprintw(0, 48, "%2d", t->size);
+    while(t->e[i] && t->e[i]->key && strcmp(t->e[i]->key, k)) {
+        t->e[i]->col = true;
+        i = COLLIDE(i,t->size);
+    }
+    mvprintw(0, 44, "%2d", i);
+    return i;
+}
+
+char *hget(Table *t, char *k) {
+    Entry *e = t->e[_get_index_by_key(t, k)];
+    return (e && e->val) ? e->val : "";
+}
+
+void hset(Table *t, char *k, char *v) {
+    Entry *e;
+    unsigned int i = _get_index_by_key(t, k);
+    if(!t->e[i]) {
+        t->e[i] = calloc(1, sizeof(Entry));
+    };
+    e = t->e[i];
+    if(!e->key) {
+        e->key = calloc(BUFSIZ, sizeof(char));
+        e->val = calloc(BUFSIZ, sizeof(char));
+        strcpy(e->key, k);
+        ++t->entries;
+    }
+    strcpy(e->val, v);
+    if(t->entries / (double)t->size > t->load) {
+        hresize(t->size * 2, t);
+    }
+}
+
+void hdel(Table *t, char *k) {
+    unsigned int i = _get_index_by_key(t, k);
+    bool col;
+    if(t->e[i] && !strcmp(t->e[i]->key, k)) {
+        col = t->e[i]->col;
+        --t->entries;
+        t->e[i] = NULL;
+        if(t->size > SIZE && t->entries / (double)(t->size / 2) < t->load) {
+            hresize(t->size / 2, t);
+        } else {
+            while(col) {
+                i = COLLIDE(i,t->size);
+                col = t->e[i]->col;
+                hset(t, t->e[i]->key, t->e[i]->val);
+                t->e[i] = NULL;
+                --t->entries;
+            }
+        }
+    }
+}
+
+void printKeys(Table *t) {
+    unsigned int s = t->size;
+    while(s--) {
+        if(t->e[s] && t->e[s]->key) {
+            printf("%s\n", t->e[s]->key);
+        }
+    }
+}
+
+void printArray(Table *t) {
+    unsigned int s = t->size;
+    while(s--) {
+        if(!t->e[s]) {
+            printf("%d: empty\n", s);
+        } else if(!t->e[s]->key) {
+            printf("%d: dummy\n", s);
+        } else {
+            printf("%d: %s, %s\n", s, t->e[s]->key, t->e[s]->val);
+        }
+    }
+    printf("\n");
+}
+
+Table *makeTable() {
+    return halloc(SIZE);
+}
+
+void freeTable(Table *t) {
+    while(t->size--) {
+        free(t->e[t->size]);
+    }
+    free(t->e);
+    free(t);
+}
---
+++ hash.h
@@ -0,0 +1,23 @@
+#ifndef __HASH
+#define __HASH
+
+typedef struct _entry {
+    char *key;
+    char *val;
+    bool col;
+} Entry;
+
+typedef struct _table {
+    unsigned int size, entries;
+    float load;
+    Entry **e;
+} Table;
+
+Table *makeTable();
+void freeTable(Table *);
+
+void hset(Table *t, char *key, char *value);
+void hdel(Table *t, char *key);
+char *hget(Table *t, char *key);
+
+#endif /* !__HASH */
